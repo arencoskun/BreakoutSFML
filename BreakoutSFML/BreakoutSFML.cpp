992ceed4d252984886fbe2205db3fc776bb4d2ae
arencoskun <ar3n1596@gmail.com>
2023-01-01 01:42:05 +0300

    Create simple window

--- BreakoutSFML/BreakoutSFML.cpp
+++ BreakoutSFML/BreakoutSFML.cpp
@@ -1,24 +1,45 @@
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+#include <iostream>
+#include <string>
 
-    while (window.isOpen())
+const int WINDOW_WIDTH = 800, WINDOW_HEIGHT = 600;
+const std::string WINDOW_TITLE = "Breakout - https://github.com/arencoskun";
+sf::Clock deltaClock;
+sf::Time deltaTime;
+
+void Update(float dt)
+{
+    std::cout << "Update. (" << dt << ")" << std::endl;
+}
+
+void Draw(sf::RenderWindow& wnd)
+{
+    wnd.clear();
+    wnd.display();
+}
+
+void GameLoop(sf::RenderWindow& wnd)
+{
+    while (wnd.isOpen())
     {
         sf::Event event;
-        while (window.pollEvent(event))
+        while (wnd.pollEvent(event))
         {
             if (event.type == sf::Event::Closed)
-                window.close();
+                wnd.close();
         }
 
-        window.clear();
-        window.draw(shape);
-        window.display();
+        Update(deltaClock.restart().asSeconds());
+        Draw(wnd);
     }
+}
+
+int main()
+{
+    sf::RenderWindow wnd(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_TITLE);
+    
+    GameLoop(wnd);
 
     return 0;
 }
